911c1a6a38662a44eddf14b1d950d754640d1b4d
AndreLuis117 <andreluis2006@outlook.com>
2021-09-17 21:12:27 -0300

    Nova camada de model para classes apresentadas nas api's

---
+++ RoomManagement/Model/Api/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Api
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime ExclusionDate { get; set; }
+    }
+}
